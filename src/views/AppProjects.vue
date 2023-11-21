496f71769a720cf4f91e671bccafb2f41627fbf7
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-11-21 17:15:07 +0100

    add loader comp and add to projects and singleproj

--- src/views/AppProjects.vue
+++ src/views/AppProjects.vue
@@ -2,6 +2,7 @@
 import axios from 'axios';
 
 import AppCard from '../components/AppCard.vue';
+import AppLoader from '../components/AppLoader.vue';
 
 export default {
     name: 'AppProjects',
@@ -9,37 +10,49 @@ export default {
         return {
             projects_api: 'http://127.0.0.1:8000/api/projects',
             projects: [],
+            loading: true,
         }
+    },
+    components: {
+        AppLoader,
     },
     mounted() {
         axios
             .get(this.projects_api)
             .then(response => {
                 this.projects = response.data.projects.data;
+                this.loading = false;
             })
     },
     components: {
         AppCard,
+        AppLoader
     }
 
 }
 </script>
 <template>
-    <div class="container">
+    <div v-if="!loading">
+        <div class="container">
 
 
 
-        <h1 class="pb-3 mb-5 border-bottom border-info">
-            My Projects
-        </h1>
+            <h1 class="pb-3 mb-5 border-bottom border-info">
+                My Projects
+            </h1>
 
-        <div class="row gy-5">
-            <div class="col-4" v-for="project in   this.projects  ">
+            <div class="row gy-5">
+                <div class="col-4" v-for="project in   this.projects  ">
 
-                <AppCard :project="project" />
+                    <AppCard :project="project" />
 
+                </div>
             </div>
         </div>
+    </div>
+
+    <div v-else>
+        <AppLoader />
     </div>
 </template>
 
